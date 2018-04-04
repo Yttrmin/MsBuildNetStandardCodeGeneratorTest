003dbb48934f2ebc1608d8da1f14071b374b1937
Yttrmin <Yttrmin@users.noreply.github.com>
2018-04-03 22:15:48 -0400

    Commenting.

--- NetStandardCodeGeneratorTest/Class1.cs
+++ NetStandardCodeGeneratorTest/Class1.cs
@@ -6,6 +6,9 @@ namespace NetStandardCodeGeneratorTest
     {
 		public string GetTestValue()
 		{
+			// Note there's no source file in this project (if freshly pulled) that contains a "GeneratedClass" class with a "TestValue" member.
+			// On compilation, a Target in the .csproj will be run that generates a source file needed for this.
+			// Afterwards, the source file will persist.
 			return GeneratedClass.TestValue;
 		}
     }
